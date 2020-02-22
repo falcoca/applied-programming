9534da257ad0e5cba5bc0ff8caba9aac9c668169
falcoca <falcoca@gmail.com>
2020-02-22 16:37:00 +0000

    week 7

--- dice.rb
+++ dice.rb
@@ -3,6 +3,32 @@ require "sinatra/reloader"
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
+# get "/" do
+#     # "Hello!"
+#     "<h1>Luke, I am your father</h1>"
+#     # "<h2>Listen to me</h2>"
+# end 
+  
+# get "/tacos" do
+#     "Tacos are awesome"
+# end
+
 get "/" do
-  "Hello"
+    view "dice"
+
+
+  @die1 = rand(1..6)
+  @die2 = rand(1..6)
+  @die3 = rand(1..6)
+  @die4 = rand(1..6)
+  @die5 = rand(1..6)
+  @dice = [@die1, @die2, @die3, @die4, @die5]
+  for die in (0..4)
+    die = rand(1..6)    
+    @dice << rand(1..6)
+    
+    "<h1>Dice: #{die1},#{die2} </h1>"
+   
+#     view "dice" 
 end
+
--- weather.rb
+++ weather.rb
@@ -1,10 +1,47 @@
 require "forecast_io"
 
 # configure the Dark Sky API with your API key
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "08648ef4aff8fe4c83ffc7d9ed45ffd4"
 
 # do the heavy lifting, use Global Hub lat/long
 forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
+# puts forecast
+
+# puts forecast.keys
+
+# In Chicago, it is currently 33.89 degress and Clear
+
+# puts forecast["currently"]
+# pp forecast["currently"]
+
+# puts forecast["currently"]["temperature"] 
+# current_summary = forecast["currently"]["summary"]
+# current_temp = forecast["currently"]["temperature"]
+
+# puts "In Chicago, it is currently #{current_temp} degrees and #{current_summary}."
+
+
+
+
+# A high temperature of 36.47 and Possible flurries in the afternoon.
+# puts forecast["daily"]
+# pp forecast["daily"]
+# pp forecast["daily"]["data"]
+# puts forecast["daily"]["data"][0]["temperatureHigh"]
+
+# # First day forecast
+# puts "A high temperature of #{forecast["daily"]["data"][0]["temperatureHigh"]} and #{forecast["daily"]["data"][0]["summary"]}"
+# # Second day forecast
+# puts "A high temperature of #{forecast["daily"]["data"][1]["temperatureHigh"]} and #{forecast["daily"]["data"][1]["summary"]}"
+# # Third day forecast...
+# puts "A high temperature of #{forecast["daily"]["data"][2]["temperatureHigh"]} and #{forecast["daily"]["data"][2]["summary"]}"
+
+
+forecast_array = forecast["daily"]["data"]
+for zebra in forecast_array
+    puts "A high temperature of #{zebra["temperatureHigh"]} and #{zebra["summary"]}"
+end 
+
 
 # pp = pretty print
 # use instead of `puts` to make reading a hash a lot easier
@@ -13,6 +50,7 @@ forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
 # Example output:
 #
 # In Chicago, it is currently 33.89 degress and Clear
+
 # Extended forecast:
 # A high temperature of 36.47 and Possible flurries in the afternoon.
 # A high temperature of 33.98 and Mostly cloudy throughout the day.
